30d603190054c50038e8ec61d1b80f45dc727aec
LitBay <LitBay@outlook.com>
2024-01-09 23:49:49 +0800

    建立dev分支，完善项目初始化

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# EmbeddedTools
-Android application for embedded development
+# 嵌入式工具箱 | EmbeddedTools
+嵌入式开发安卓应用工具箱 | Android application for embedded development
+
+### 功能
+
+- 正在开发蓝牙功能...
---
+++ app/src/main/java/com/github/litbay/embeddedtools/ui/pages/rootPage.kt
@@ -0,0 +1,16 @@
+package com.github.litbay.embeddedtools.ui.pages
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
+@Composable
+fun RootPage(){
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        //
+    }
+}
